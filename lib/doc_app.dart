50ecfd102e485d637424380b75005acc1f371bff
mohRamy <mohammedrdaly@gmail.com>
2024-01-07 16:30:34 +0000

    new udpate

---
+++ lib/doc_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,13 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced/doc_app.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_advanced/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DocApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
