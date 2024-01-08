d6ac7272624af03e22b60bd8a90654f353eac3ec
mohRamy <mohammedrdaly@gmail.com>
2024-01-08 15:47:07 +0000

    - added text style - added onboarding screen

---
+++ lib/src/core/theme/text_style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font24Black700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32.sp, 
+    fontWeight: FontWeight.bold,
+    color: Colors.blue,
+  );
+}
